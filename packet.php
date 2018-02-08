e51f9373b1c094f9616ad388e1634ae408d596b6
meldianing <35475106+meldianing@users.noreply.github.com>
2018-02-08 10:12:10 +0800

    Update packet.php

--- packet.php
+++ packet.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" scr="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
@@ -13,7 +13,7 @@
 			<tr>
 				
 				<li><label class="titlePage"> Networking </label>
-					<img src="img/hb.gif" width="10%" height="5%"></a><br>
+					<img src="hb.gif" width="10%" height="5%"></a><br>
 			</li>
 		</div>
 			</tr>
@@ -32,23 +32,23 @@
 				<td>
 				<div class="desc">
   SAMPLE *** .</div>
-  				<img id = "pic" src="../img/44.png" width="50%" height="40%">
+  				<img id = "pic" src="44.png" width="50%" height="40%">
   				<div class="desc">
   opening the packet tracer *** .</div>
-				<img id = "pic" src="../img/11.png" width="50%" height="40%">
+				<img id = "pic" src="11.png" width="50%" height="40%">
 				<div class="desc">
   This is the input of the packet tracer
  
 *** .</div>
 				</td>
-				<img id = "pic" src="../img/22.png" width="50%" height="40%">
+				<img id = "pic" src="22.png" width="50%" height="40%">
 			</td>
   <div class="desc">
   this is the sample of the tools
  
 *** .</div>
 <div>
-			<li><a class = "Images" href="vid.php" id = "Images" href="">click here to view more<img src="img/cc.gif" width="50%" height="50%"></a><br>
+			<li><a class = "Images" href="vid.php" id = "Images" href="">click here to view more<img src="cc.gif" width="50%" height="50%"></a><br>
 			</li>
 		</div>
 </div>
@@ -56,6 +56,6 @@
 
 
 	<tr></tr>
-	<img id = "img" src="../img/02.jpg" width="100%" height="30%" >
+	<img id = "img" src="02.jpg" width="100%" height="30%" >
 </body>
 </html>
